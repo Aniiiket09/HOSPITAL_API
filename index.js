1029adfe4fd2e5985a6057eda5ff80420031a250
aniket_tarapurkar <aniket.v.tarapurkar99@gmail.com>
2023-06-02 12:06:25 +0530

    import db in our main index.js file

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const port = 9000;
+const db = require("./config/mongoose");
+const app = express();
 
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.listen(port, function (err) {
   if (err) {
     console.log("error");
